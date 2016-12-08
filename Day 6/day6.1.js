ba9e4ece05077703ccff482bd086bf8b2d3b9bd7
Marcus Carey <mcarey1590@gmail.com>
2016-12-07 19:00:07 -0600

    day 6

---
+++ Day 6/day6.1.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var input = fs.readFileSync('day6Input.txt').toString().trim().split('\n');
+
+var columns = [];
+
+input.forEach(function(line){
+    for(var i = 0; i < line.length; i++){
+        if(!columns[i]){ columns[i] = {}; }
+        var letter = line.charAt(i);
+        if(!columns[i][letter]){ 
+            columns[i][letter] = 1; 
+        } else {
+            columns[i][letter] += 1;
+        }
+    }
+});
+
+var message = '';
+columns.forEach(function(column){
+    var colLetter;
+    var occurences = 0;
+    for(var letter in column){
+        if(column[letter] > occurences){
+            colLetter = letter;
+            occurences = column[letter];
+        }
+    }
+    message += colLetter;
+});
+
+console.log(message);
